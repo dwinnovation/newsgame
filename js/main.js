425e496640d1662f4481920d99765f2b773d9bfa
dwinnovation <hannes.rohde@dw.de>
2014-04-15 14:44:37 +0200

    js quiz
    enable quiz questions

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-
+$(document).ready(
+	function() {
+		// find and "enable" all quiz questions:
+		$('[id^="quizquestion_"]').each(
+			function(i, $element) {
+				alert($element.attr("id"));
+			}
+		);
+	}
+);
