f5de276688f83fc8089edd96166fa1e996835177
dwinnovation <hannes.rohde@dw.de>
2014-04-15 15:42:17 +0200

    question activation

--- js/main.js
+++ js/main.js
@@ -1,11 +1,92 @@
+var answers = {
+	quizquestion_1 : {
+		questionType : "textWithImage",
+		answersOptions : [
+			{
+				text : "Sowjetunion",
+			  	image : "http:/www.bla.de/dkjbvjhdb",
+			  	correct: true
+			},
+			{
+				text : "DDR",
+			 	image : "http:/www.bla.de/dkjbvjhdb"
+			},
+			{
+				text : "Aserbaidschan",
+			 	image : "http:/www.bla.de/dkjbvjhdb"
+			}
+		]
+	},
+	quizquestion_2 : {
+		questionType : "text",
+		answerOptions : [
+			{
+			 	text : "Ursula von der Leyen",
+			  	correct: true
+			},
+			{
+				text : "Angela Merkel"
+			},
+			{
+				text : "Hillary Clinton"
+			}
+		]
+	}
+};
+
+function buildQuestionContent(questionId) {
+	var answer = answers[questionId];
+
+	if (typeof answer === 'undefined') {
+		return "no question definition!";
+	}
+
+	var $ul = $("<ul>");
+
+	if (answer.questionType == "text") {
+		$.each(answer.answerOptions, function(i, opt) {
+			var $li = $("<li>").text(opt.text);
+			if (opt.correct)
+			{
+				$li.addClass("correct");
+			}
+			
+			$ul.append($li);
+		});
+	} else if (answer.questionType == "textWithImage") {
+		$.each(answer.answerOptions, function(i, opt) {
+			var $li = $("<li>").text(opt.text);
+			if (opt.correct)
+			{
+				$li.addClass("correct");
+			}
+			
+			$ul.append($li);
+		});
+	} else {
+	}
+	
+	var content = $("<div>").append($ul).html();
+	
+	return content;
+}
+
 function enableQuizQuestion($span) {
+	
+	var spanId = $span.attr('id');
+	var content = buildQuestionContent(spanId);
+
+	// remove text:
+	$span.text("");
+	
+	console.log(content);
+	
 	$span.popover(
 	{
-		html: true,
-		placement: "auto",
-		trigger: "click",
-		title: "Frage",
-		content: "content",
+		"html": true,
+		"placement": "auto",
+		"trigger": "click",
+		"content": content
 	}
 	);
 }
